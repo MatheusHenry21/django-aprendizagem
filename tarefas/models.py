bfa8ed52aa59e10f27de147e865eee44c2d0e779
Matheus Henrique <teus.henrique021@gmail.com>
2026-01-16 13:32:48 -0300

    Criação de DB

--- tarefas/models.py
+++ tarefas/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class TarefaModel(models.Model):
+    nome = models.CharField(max_length=50)
+    descricao = models.TextField(null=True, blank=True)
+    situacao = models.BooleanField(default=False)
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nome
